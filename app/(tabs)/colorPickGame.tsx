f80fee8a3e7b082987c4c9f4e746904215ad8a8d
andyerdene <andyerdene@gmail.com>
2024-07-31 17:00:08 +0800

    color pick game

--- app/(tabs)/colorPickGame.tsx
+++ app/(tabs)/colorPickGame.tsx
@@ -13,22 +13,41 @@ import * as MediaLibrary from "expo-media-library";
 import { Image } from "expo-image";
 
 const getRandomColor = (difference: number) => {
-  return `rgb(200,${200 - difference},150)`;
+  const red = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const randomColor = `rgb(${red},${green},${blue})`;
+  const differentColor = `rgb(${red},${
+    green > 100 ? green - difference : green + difference
+  },${blue})`;
+  return {
+    color: randomColor,
+    differentColor: differentColor,
+  };
+};
+
+const getRandomNum = () => {
+  return Math.floor(Math.random() * 9) + 1;
 };
 export default function App() {
+  const [level, setLevel] = useState(50);
+  const [color, setColor] = useState(getRandomColor(level));
+  const [selectedSquare, setSelectedSquare] = useState(getRandomNum());
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.gameContainer}>
         {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((item) => {
           return (
             <Pressable
+              key={item}
               style={{
                 ...styles.square,
                 backgroundColor:
-                  item == 5 ? getRandomColor(10) : getRandomColor(0),
+                  item == selectedSquare ? color.color : color.differentColor,
               }}
             >
-              {item}
+              <Text>{item}</Text>
             </Pressable>
           );
         })}
